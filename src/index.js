2105059991ead1150d13cdc8879b1e1b810a79cb
Haoyang Zhang <howhowhathere@gmail.com>
2022-10-06 13:02:28 -0700

    from the reset work... so this is base code

---
+++ src/index.js
@@ -0,0 +1,405 @@
+////////////////////////////
+// DO NOT EDIT THIS ARRAY //
+////////////////////////////
+/*
+ The contacts array contain the list of contacts for the contact book.
+*/
+window.contacts = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    picture: "https://via.placeholder.com/150/008600/FFFFFF?text=LG",
+    address: {
+      street: "Kulas Light",
+      suite: "Apt. 556",
+      city: "Green Vale",
+      zipcode: "92998-3874",
+      geo: {
+        lat: "-37.3159",
+        lng: "81.1496",
+      },
+    },
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org",
+    company: {
+      name: "Romaguera-Crona",
+      catchPhrase: "Multi-layered client-server neural-net",
+      bs: "harness real-time e-markets",
+    },
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    email: "Shanna@melissa.tv",
+    picture: "https://via.placeholder.com/150/0000FF/FFFFFF?text=EH",
+    address: {
+      street: "Victor Plains",
+      suite: "Suite 879",
+      city: "Blue Vale",
+      zipcode: "90566-7771",
+      geo: {
+        lat: "-43.9509",
+        lng: "-34.4618",
+      },
+    },
+    phone: "010-692-6593 x09125",
+    website: "anastasia.net",
+    company: {
+      name: "Deckow-Crist",
+      catchPhrase: "Proactive didactic contingency",
+      bs: "synergize scalable supply-chains",
+    },
+  },
+  {
+    id: 3,
+    name: "Clementine Bauch",
+    email: "Nathan@yesenia.net",
+    picture: "https://via.placeholder.com/150/34F500/FFFFFF?text=CB",
+    address: {
+      street: "Douglas Extension",
+      suite: "Suite 847",
+      city: "Green Vale",
+      zipcode: "59590-4157",
+      geo: {
+        lat: "-68.6102",
+        lng: "-47.0653",
+      },
+    },
+    phone: "1-463-123-4447",
+    website: "ramiro.info",
+    company: {
+      name: "Romaguera-Jacobson",
+      catchPhrase: "Face to face bifurcated interface",
+      bs: "e-enable strategic applications",
+    },
+  },
+  {
+    id: 4,
+    name: "Patricia Lebsack",
+    email: "Julianne.OConner@kory.org",
+    picture: "https://via.placeholder.com/150/008600/FFFFFF?text=PL",
+    address: {
+      street: "Hoeger Mall",
+      suite: "Apt. 692",
+      city: "South Vale",
+      zipcode: "53919-4257",
+      geo: {
+        lat: "29.4572",
+        lng: "-164.2990",
+      },
+    },
+    phone: "493-170-9623 x156",
+    website: "kale.biz",
+    company: {
+      name: "Robel-Corkery",
+      catchPhrase: "Multi-tiered zero tolerance productivity",
+      bs: "transition cutting-edge web services",
+    },
+  },
+  {
+    id: 5,
+    name: "Chelsey Dietrich",
+    email: "Lucio_Hettinger@annie.ca",
+    picture: "https://via.placeholder.com/150/FF00FF/FFFFFF?text=CD",
+    address: {
+      street: "Skiles Walks",
+      suite: "Suite 351",
+      city: "North Vale",
+      zipcode: "33263",
+      geo: {
+        lat: "-31.8129",
+        lng: "62.5342",
+      },
+    },
+    phone: "(254)954-1289",
+    website: "demarco.info",
+    company: {
+      name: "Keebler LLC",
+      catchPhrase: "User-centric fault-tolerant solution",
+      bs: "revolutionize end-to-end systems",
+    },
+  },
+  {
+    id: 6,
+    name: "Mrs. Dennis Schulist",
+    email: "Karley_Dach@jasper.info",
+    picture: "https://via.placeholder.com/150/000086/FFFFFF?text=DS",
+    address: {
+      street: "Norberto Crossing",
+      suite: "Apt. 950",
+      city: "South Vale",
+      zipcode: "23505-1337",
+      geo: {
+        lat: "-71.4197",
+        lng: "71.7478",
+      },
+    },
+    phone: "1-477-935-8478 x6430",
+    website: "ola.org",
+    company: {
+      name: "Considine-Lockman",
+      catchPhrase: "Synchronised bottom-line interface",
+      bs: "e-enable innovative applications",
+    },
+  },
+  {
+    id: 7,
+    name: "Kurtis Weissnat",
+    email: "Telly.Hoeger@billy.biz",
+    picture: "https://via.placeholder.com/150/860000/FFFFFF?text=KW",
+    address: {
+      street: "Rex Trail",
+      suite: "Suite 280",
+      city: "Blue Vale",
+      zipcode: "58804-1099",
+      geo: {
+        lat: "24.8918",
+        lng: "21.8984",
+      },
+    },
+    phone: "210.067.6132",
+    website: "elvis.io",
+    company: {
+      name: "Johns Group",
+      catchPhrase: "Configurable multimedia task-force",
+      bs: "generate enterprise e-tailers",
+    },
+  },
+  {
+    id: 8,
+    name: "Nicholas Runolfsdottir V",
+    email: "Sherwood@rosamond.me",
+    picture: "https://via.placeholder.com/150/FF0000/FFFFFF?text=NR",
+    address: {
+      street: "Ellsworth Summit",
+      suite: "Suite 729",
+      city: "Green Vale",
+      zipcode: "45169",
+      geo: {
+        lat: "-14.3990",
+        lng: "-120.7677",
+      },
+    },
+    phone: "586.493.6943 x140",
+    website: "jacynthe.com",
+    company: {
+      name: "Abernathy Group",
+      catchPhrase: "Implemented secondary concept",
+      bs: "e-enable extensible e-tailers",
+    },
+  },
+  {
+    id: 9,
+    name: "Glenna Reichert",
+    email: "Chaim_McDermott@dana.io",
+    picture: "https://via.placeholder.com/150/226688/FFFFFF?text=GR",
+    address: {
+      street: "Dayna Park",
+      suite: "Suite 449",
+      city: "White Vale",
+      zipcode: "76495-3109",
+      geo: {
+        lat: "24.6463",
+        lng: "-168.8889",
+      },
+    },
+    phone: "(775)976-6794 x41206",
+    website: "conrad.com",
+    company: {
+      name: "Yost and Sons",
+      catchPhrase: "Switchable contextually-based project",
+      bs: "aggregate real-time technologies",
+    },
+  },
+  {
+    id: 10,
+    name: "Clementina DuBuque",
+    email: "Rey.Padberg@karina.biz",
+    picture: "https://via.placeholder.com/150/000000/FFFFFF?text=CD",
+    address: {
+      street: "Kattie Turnpike",
+      suite: "Suite 198",
+      city: "Blue Vale",
+      zipcode: "31428-2261",
+      geo: {
+        lat: "-38.2386",
+        lng: "57.2232",
+      },
+    },
+    phone: "024-648-3804",
+    website: "ambrose.net",
+    company: {
+      name: "Hoeger LLC",
+      catchPhrase: "Centralized empowering task-force",
+      bs: "target end-to-end models",
+    },
+  },
+];
+
+///////////////////////////////////
+// WRITE YOUR SOLUTION CODE HERE //
+///////////////////////////////////
+
+//=========================== render contact instructions======================
+//renderContact()
+
+// This function accepts a contact object in the format described above.
+// Create and return the HTML to render a single contact. The HTML for a single contact should look like this:
+
+// <div class="card" data-id="39">
+//   <button class="deleteBtn" title="Delete this contact">X</button>
+//   <div class="avatar">
+//     <div class="circle"></div>
+//     <div class="circle"></div>
+//     <img src="https://via.placeholder.com/150/FE0F0B/FFFFFF?Text=AU" />
+//   </div>
+//   <div class="info">
+//     <span class="name big">Arwen Undómiel</span>
+//     <span class="email small">arwen@imladris.com</span>
+//   </div>
+//   <div class="details">
+//     <div class="phone">010-692-6593 x09125</div>
+//     <div class="website">arwen.com</div>
+//   </div>
+
+//   <div class="additional">
+//     <div class="address">
+//       <div class="suite">Suite 17</div>
+//       <div class="street">Main Street</div>
+//       <div class="city">Imladris, 90566-7771</div>
+//     </div>
+//     <div class="company">
+//       <div class="label">Works at</div>
+//       <div class="company-name">Eriador Services</div>
+//     </div>
+//   </div>
+// </div>
+
+// Note: The tests are looking for these specific elements and class names to be used. Be sure to check your spelling properly.
+
+/*
+ Create and return the HTML to render a single contact card.
+ The `contact` parameter is an object representing a single contact. 
+*/
+function renderContact(contact) {}
+
+// ================== render instructions =====================
+// render()
+
+// This method accepts an array of contacts.
+// Render the array of contacts and insert them into the DOM.
+
+// Additionally, call the loadCities() function to ensure that the list of cities in the filter is updated.
+
+// The contacts should be rendered in the section with the ID contacts. If there are no contacts in the list, simply leave the section empty.
+
+/*
+  Render the array of contacts and insert them on the DOM.
+  The contacts should be rendered in the `section` with id "contacts".
+*/
+function render(contacts) {}
+
+// ================= filterByCity=================
+// filterByCity()
+
+// Accepts a string representing the name of a city. Filter the list of contacts by contacts that live in the given city.
+// Create a new array for the filtered contact. DO NOT MODIFY THE ORIGINAL CONTACTS ARRAY. Return the filtered contacts.
+
+/*
+  Filter by city. Filter the  array of contacts by the given city.
+  Return a new array containing the filtered list. 
+  Do NOT modify the original array.
+*/
+function filterByCity(city) {}
+
+//===================== filterhandler instructions ====
+// filterHandler()
+
+// This function attaches a change event listener to the filterOptions select.
+// On change, get the value of the select that was selected.
+// If the value is 0, then call render() with the full list of contacts.
+// Otherwise, call filterByCity() with the city that the user selected, then call render with the filtered list produced.
+
+/*
+  Add an `change` event listener to the `filterOptions` select element.
+  On `change` get the value selected by the user. 
+  If the value is "0" call `render()` with the complete contacts list.
+  If the value is not "0" call `filterByCity()` passing the value selected by
+  the user. Then call `render()` with the filtered list.
+*/
+function filterHandler() {}
+
+// ======== load Cities =====
+// loadCities()
+
+// Accepts an array of contacts.
+// The loadCities() function finds all unique cities in the contact list and inserts a set of options in the select element with the ID filterOptions. The default option should remain in place.
+
+// After populating the filterOptions select, it should look like this:
+
+// <select name="filterOptions" id="filterOptions">
+//   <option value="0">-- Select a city --</option>
+//   <option value="New York">New York</option>
+//   <option value="Kinsasha">Kinsasha</option>
+// </select>
+
+/*
+  Accepts an array of contacts.
+  Populate the select with id `filterOptions` with the list of cities.
+  Create a list of cities from the contacts array with no duplicates then
+  add an `<option>` element for each city to the select.
+*/
+function loadCities(contacts) {}
+
+//================= deleteContact ==================
+// deleteContact()
+
+// Accepts an ID representing the ID of a contact in the contact list.
+// Remove the contact with the given ID from the contact list. If the ID doesn't exist, then no contacts are deleted.
+
+/*
+  Remove the contact from the contact list with the given id.
+*/
+function deleteContact(id) {}
+
+//========================= Delete Button Handler instructions================
+
+// deleteButtonHandler()
+
+// Add a click event handler to the deleteBtn elements.
+// When clicked, get the ID of the card that was clicked from the corresponding data-id. Then call deleteContact() and re-render the list.
+/*
+  Add a `click` event handler to the `deleteBtn` elements.
+  When clicked, get the id of the card that was clicked from the 
+  corresponding `data-id` then call `deleteContact()` and re-render 
+  the list.
+*/
+function deleteButtonHandler() {}
+
+//==================== main ==========
+// main()
+
+// This is the starting point of the code. Call the necessary functions to add event listeners. Then render the contacts.
+
+/*
+  Perform all startup tasks here. Use this function to attach the 
+  required event listeners, call loadCities() then call render().
+*/
+function main() {}
+
+window.addEventListener("DOMContentLoaded", main);
+
+//////////////////////////////////////////
+// DO NOT EDIT ANYTHING BELOW THIS LINE //
+//////////////////////////////////////////
+window.contacts = contacts;
+window.renderContact = renderContact;
+window.render = render;
+window.filterByCity = filterByCity;
+window.filterHandler = filterHandler;
+window.loadCities = loadCities;
+window.deleteContact = deleteContact;
+window.deleteButtonHandler = deleteButtonHandler;
+window.main = main;
